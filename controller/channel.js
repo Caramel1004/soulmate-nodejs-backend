d8f5c7d29332a5cbf6372f9bcf0a09210fb18cbc
Caramel1004 <Caramel1004.js@gmail.com>
2023-05-15 16:02:28 +0900

    채널 입장, 채널 퇴장 기능 추가

--- controller/channel.js
+++ controller/channel.js
@@ -1,19 +1,20 @@
 import Channel from '../models/channel.js';
+import user from '../models/user.js';
 import User from '../models/user.js'
 
 const channelController = {
-    // 채널 리스트 조회
+    // 해당 유저의 채널 리스트 조회
     getChannelList: async (req, res, next) => {
         try {
             const user = await User.findById('645bc55b7d8b60a0021cb1b5').populate('channels');//poplate함수로 해당아이디에대한 채널정보 모두 가져오기
-            console.log(user)
+            // console.log(user)
             if (!user) {
                 const error = new Error('채널리스트를 불러오지 못했습니다.');
                 error.statusCode = 401;
                 throw error;
             } else {
                 const channels = user.channels;
-                console.log(channels);
+                // console.log(channels);
                 res.status(200).json({
                     msg: '채널 리스트 입니다.',
                     channels: channels
@@ -30,15 +31,32 @@ const channelController = {
     getChannelById: async (req, res, next) => {
         try {
             const channelId = req.params.channelId;
-            const channel = await Channel.findById(channelId);
-            if (!channel) {
+
+            // 1. 유저가 해당 채널의 아이디를 가지고 있는지 부터 체크 없으면 에러 스루
+            const userInfo = await User.findById('645bc55b7d8b60a0021cb1b5');// 나중에는 토큰으로 유저 유무 구분 할거임
+            const userChannelIdList = userInfo.channels;
+
+            const matchedChannel = userChannelIdList.find(userChId => userChId.toString() === channelId.toString());
+
+            if (!matchedChannel) {
                 const error = new Error('해당채널을 찾지 못했습니다.');
-            } else {
-                res.status(200).json({
-                    msg: '채널 입장 성공 입니다.',
-                    channel: channel
-                })
+                error.statusCode = 401;
+                throw error;
             }
+
+            // 2. 유저가 해당 채널의 아이디를 자기고있으면 다음 채널아이디로 해당채널 조회
+            const finalChannel = await Channel.findById(matchedChannel._id);
+
+            if (!finalChannel) {
+                const error = new Error('해당채널을 찾지 못했습니다.');
+                error.statusCode = 401
+                throw error;
+            }
+            res.status(200).json({
+                msg: '채널 입장 성공 입니다.',
+                channel: finalChannel
+            })
+
         } catch (err) {
             if (!err.statusCode) {
                 err.statusCode = 500;
@@ -52,11 +70,13 @@ const channelController = {
             // 채널 이벤트 발생
             // 1. 채널 생성 2. 해당유저 소유자로 지정 3. 참여 채널 저장 4. 해당유저의 채널에 추가
             const channelName = req.body.channelName;
-            const thumbnail = req.body.thumbnail;
+            let thumbnail = req.body.thumbnail;
             const matchedUser = await User.findById('645bc55b7d8b60a0021cb1b5');
-            
-            console.log(matchedUser)
 
+            console.log('thumbnail: ',thumbnail);
+            if(thumbnail === ''){
+                thumbnail = 'https://d2u3dcdbebyaiu.cloudfront.net/uploads/atch_img/309/59932b0eb046f9fa3e063b8875032edd_crop.jpeg';
+            }
             const owner = {
                 ownerId: matchedUser._id,
                 ownerName: matchedUser.name
@@ -78,6 +98,40 @@ const channelController = {
                 msg: '채널이 생성되었습니다.',
                 channel: createChannel
             });
+        } catch (err) {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        }
+    },
+    // 해당 채널에서 퇴장
+    patchExitChannel: async (req, res, next) => {
+        try {
+            // 1. 채널스키마에서 해당 유저 삭제
+            const channelId = req.body.channelId;
+            console.log('channelId: ',channelId);
+            const userId = '645bc55b7d8b60a0021cb1b5';
+            const matchedChannel = await Channel.findById(channelId);
+
+            const updatedUsers = matchedChannel.users.filter(id => id.toString() !== userId.toString());
+
+            matchedChannel.users = [...updatedUsers];
+
+            console.log('updatedUsers: ', updatedUsers);
+            await matchedChannel.save();
+
+            // 2. 유저스키마에서 해당 채널 삭제
+            const exitedUser = await User.findById('645bc55b7d8b60a0021cb1b5');
+            const updatedChannels = exitedUser.channels.filter(id => id.toString() !== channelId.toString());
+
+            exitedUser.channels = [...updatedChannels];
+            await exitedUser.save();
+
+            res.status(200).json({
+                msg:'해당 유저가 채널에서 퇴장하였습니다.',
+                exitedUser: exitedUser
+            })
         } catch (err) {
             if (!err.statusCode) {
                 err.statusCode = 500;
--- routes/channel.js
+++ routes/channel.js
@@ -4,12 +4,15 @@ import channelController from '../controller/channel.js';
 const router = Router();
 
 // GET /v1/channel
-router.get('/', channelController.getChannelList);
+router.get('/', channelController.getChannelList);// 모든 채널 조회
 
 //GET /v1/channel/:channelId
-router.get('/:channelId', channelController.getChannelById);
+router.get('/:channelId', channelController.getChannelById);// 해당 채널에 접속
 
 //POST /v1/channel/create
-router.post('/create',channelController.postCreateChannel);
+router.post('/create',channelController.postCreateChannel);// 채널 생성
+
+//GET /v1/channel/exit/:channelId
+router.patch('/exit/:channelId',channelController.patchExitChannel);// 채널 퇴장
 
 export default router;
