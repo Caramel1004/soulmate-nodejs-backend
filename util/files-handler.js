f0e30d153a02f6856c3b8701fcfda3a981295c42
Caramel1004 <Caramel1004.js@gmail.com>
2023-10-17 23:29:55 +0900

    채팅방 파일함 리스트 조회 라우터 추가

--- util/files-handler.js
+++ util/files-handler.js
@@ -88,25 +88,28 @@ const filesHandler = {
     // 3. 채널 썸네일 이미지 파일 핸들러
     saveUploadedChannelThumbnail: async (req, res, next) => {
         try {
+            console.log(req.body);
             const { thumbnail } = req.body;
             req.body.fileUrls = [];
+            if (thumbnail != 'undefined' && thumbnail!='null') {
+                console.log('진입');
+                // JSON형태로 되어있는 file 객체를 파싱하는 과정 -> buffer 프로퍼티의 data프로퍼티(배열 형태)값을 버퍼로 변환
+                const parsedThumbnail = JSON.parse(thumbnail, (key, value) => {
+                    const parsedJson = value && value.type === 'Buffer' ? Buffer.from(value) : value;
+                    return parsedJson;
+                });
 
-            // JSON형태로 되어있는 file 객체를 파싱하는 과정 -> buffer 프로퍼티의 data프로퍼티(배열 형태)값을 버퍼로 변환
-            const parsedThumbnail = JSON.parse(thumbnail, (key, value) => {
-                const parsedJson = value && value.type === 'Buffer' ? Buffer.from(value) : value;
-                return parsedJson;
-            });
-            
-            // 두번째인수에 파일을 버퍼형태로 넣어줘야 파일 볼 수 있음
-            const fileId = new Date().getTime().toString(36);
-            const mimeType = parsedThumbnail.mimetype.split('/')[1];
+                // 두번째인수에 파일을 버퍼형태로 넣어줘야 파일 볼 수 있음
+                const fileId = new Date().getTime().toString(36);
+                const mimeType = parsedThumbnail.mimetype.split('/')[1];
 
-            fs.writeFileSync(`images/channel_thumbnail/soulmate_Photo_${fileId}.${mimeType}`, parsedThumbnail.buffer, () => {
-                console.log('파일 생성');
-            });
-            req.body.fileUrl = `http://localhost:8080/images/channel_thumbnail/soulmate_Photo_${fileId}.${mimeType}`;
-            
-            console.log(req.body.fileUrl);
+                fs.writeFileSync(`images/channel_thumbnail/soulmate_Photo_${fileId}.${mimeType}`, parsedThumbnail.buffer, () => {
+                    console.log('파일 생성');
+                });
+                req.body.fileUrl = `http://localhost:8080/images/channel_thumbnail/soulmate_Photo_${fileId}.${mimeType}`;
+
+                console.log(req.body.fileUrl);
+            }
             next();
         } catch (error) {
             next(error);
