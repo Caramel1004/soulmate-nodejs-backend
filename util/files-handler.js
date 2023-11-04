3944778acdf9caa8cdb973438bfb55daecbea613
Caramel1004 <Caramel1004.js@gmail.com>
2023-11-04 23:30:07 +0900

    파일 핸들러 수정

--- util/files-handler.js
+++ util/files-handler.js
@@ -31,24 +31,30 @@ const filesHandler = {
         try {
             const { files } = req.body;
             req.body.fileUrls = [];
-
-            // JSON형태로 되어있는 file 객체를 파싱하는 과정 -> buffer 프로퍼티의 data프로퍼티(배열 형태)값을 버퍼로 변환
-            const parsedFiles = JSON.parse(files, (key, value) => {
-                const parsedJson = value && value.type === 'Buffer' ? Buffer.from(value) : value;
-                return parsedJson;
-            });
-
-            console.log('parsedFiles: ', parsedFiles);
-            // 두번째인수에 파일을 버퍼형태로 넣어줘야 파일 볼 수 있음
-            for (const parsedFile of parsedFiles) {
-                const fileId = new Date().getTime().toString(36);
-                const mimeType = parsedFile.mimetype.split('/')[1];
-
-                fs.writeFileSync(`files/soulmate_Photo_${fileId}.${mimeType}`, parsedFile.buffer, () => {
-                    console.log('파일 생성');
+            if(req.body.existFileUrls) {
+                for(const fileUrl of req.body.existFileUrls) {
+                    req.body.fileUrls.push(fileUrl.name);
+                }
+            }
+            if(files.length > 0) {
+                // JSON형태로 되어있는 file 객체를 파싱하는 과정 -> buffer 프로퍼티의 data프로퍼티(배열 형태)값을 버퍼로 변환
+                const parsedFiles = JSON.parse(files, (key, value) => {
+                    const parsedJson = value && value.type === 'Buffer' ? Buffer.from(value) : value;
+                    return parsedJson;
                 });
-
-                req.body.fileUrls.push(`files/soulmate_Photo_${fileId}.${mimeType}`);
+    
+                console.log('parsedFiles: ', parsedFiles);
+                // 두번째인수에 파일을 버퍼형태로 넣어줘야 파일 볼 수 있음
+                for (const parsedFile of parsedFiles) {
+                    const fileId = new Date().getTime().toString(36);
+                    const mimeType = parsedFile.mimetype.split('/')[1];
+    
+                    fs.writeFileSync(`files/soulmate_Photo_${fileId}.${mimeType}`, parsedFile.buffer, () => {
+                        console.log('파일 생성');
+                    });
+    
+                    req.body.fileUrls.push(`files/soulmate_Photo_${fileId}.${mimeType}`);
+                }
             }
             console.log(req.body.fileUrls);
             next();
