7173f54e05d1acc5349487bfea140319a0391b39
Caramel1004 <Caramel1004.js@gmail.com>
2023-11-12 16:41:24 +0900

    API 기능 정리

--- routes/channel.js
+++ routes/channel.js
@@ -8,104 +8,173 @@ import channelController from '../controller/channel.js';
 
 const router = Router();
 
-/**
- * # 채널 입장 전
- * 1. 오픈 채널 목록 조회
- *      1-1. 오픈 채널 박스 클릭 -> 오픈 채널 세부정보 페이지
- *      1-2. 오픈 채널 찜 클릭 -> 관심채널에 추가
- * 2. 해당 유저의 채널 리스트 조회
- *      분류 목록: 내가만든 채널, 초대 받은 채널, 오픈 채널 -> 필터
- *      - 업무(비공개) 채널 조회
- *      - 초대 받은 채널
- *      - 오픈 채널 조회
- * 3. 채널 생성
- * 4. 관심 채널 조회 
- * 5. 관심 채널 삭제
- * 
- * #채널 입장 후
- * 6. 채널아이디로 해당 채널 조회 -> 채널 세부페이지
- *      6-1. 채널에 팀원 초대 -> 초대 메세지 전송
- *      6-2. 해당 채널에서 퇴장
- * 7. 워크스페이스 목록 조회
- *      2-1. 워크 스페이스 목록중 하나 클릭 -> 세부정보 로딩
- *      2-2. 게시물 로딩
- * 8. 채팅방 목록 조회
- *      8-1. 채팅 방 입장 -> 채팅 히스토리 로딩 -> chat스키마
- * 9. 채팅룸 생성
+/** ##API 기능 정리
+ * 1. POST /v1/channel/openchannel-list: 검색키워드에의한 오픈 채널 검색 및 조회
+ * 2. GET /v1/channel/openchannel-list/:channelId: 오픈 채널 세부정보 조회
+ * 3. PATCH /v1/channel/add-or-remove-wishchannel: 관심채널 추가 또는 삭제
+ * 4. GET /v1/channel/mychannels: 해당유저의 채널 목록 조회
+ * 5. POST /v1/channel/create: 채널 생성
+ * 6. POST /v1/channel/wishchannels: 해당유저의 관심채널 목록 조회
+ * 7. GET /v1/channel/:channelId: 해당채널의 세부정보 조회
+ * 8. PATCH /v1/channel/invite/:channelId: 해당 채널에 유저 초대
+ * 9. POST /v1/channel/:channelId/chat: 해당채널에서 유저가 속한 채팅룸 검색키워드로 목록 검색
+ * 10.POST /v1/channel/:channelId/create-chatRoom: 해당채널에 채팅룸 생성
+ * 11.POST /v1/channel/:channelId/create-workspace: 해당채널에 워크스페이스 생성
+ * 12.POST /v1/channel/:channelId/workspace: 해당채널에서 유저가 속한 워크스페이스 검색키워드로 목록 검색
+ * 13.PATCH /v1/channel/exit/:channelId: 해당채널 퇴장
+ * 14.PATCH /v1/channel/create-feed/:channelId: 해당채널에 내 피드 생성
+ * 15.PATCH /v1/channel/edit-feed/:channelId/:feedId: 해당채널에 내 피드 수정
+ * 16.DELETE /v1/channel/delete-feed/:channelId/:feedId: 해당채널에 내 피드 삭제
+ * 17.PATCH /v1/channel/plus-or-minus-feed-like: 피드 좋아요수 증가 또는 감소
  */
 
-//GET /v1/channel/openchannel-list
-router.post('/openchannel-list', channelController.getSearchOpenChannelListBySearchKeyWord);//1. 오픈 채널 목록 조회
 
-// GET /v1/channel/open-channels-search
-// router.get('/open-channels-search', channelController.getSearchChannelListBySearchKeyWord);// 오픈 채널 검색
+/** POST /v1/channel/openchannel-list
+ * @method{POST}
+ * @route {/v1/channel/openchannel-list}
+ * 1. 검색키워드에의한 오픈 채널 검색 및 조회
+*/
+router.post('/openchannel-list', channelController.getSearchOpenChannelListBySearchKeyWord);
 
-// GET /v1/channel/my-channels-search
-// router.get('/my-channels-search', channelController.getSearchChannelListBySearchKeyWord);// 내 채널 검색
 
-//GET /v1/channel/openchannel-list/:channelId
-router.get('/openchannel-list/:channelId', channelController.getOpenChannelDetail);// 1-1. 오픈 채널 세부 정보 조회
+/** GET /v1/channel/openchannel-list/:channelId
+ * @method{GET}
+ * @route {/v1/channel/openchannel-list/:channelId}
+ * 2. 오픈 채널 세부정보 조회
+ */
+router.get('/openchannel-list/:channelId', channelController.getOpenChannelDetail);
 
-//PATCH /v1/channel/add-or-remove-wishchannel
-router.patch('/add-or-remove-wishchannel', hasJsonWebToken, channelController.patchAddOrRemoveWishChannel);// 1-2. 오픈 채널 찜 클릭 -> 함수 수정 -> 1-2. 관심채널 추가 또는 삭제(토글 관계)
 
-// GET /v1/channel/mychannels
-router.get('/mychannels', hasJsonWebToken, channelController.getChannelListByUserId);// 2. 해당 유저의 채널 리스트 조회
+/** PATCH /v1/channel/add-or-remove-wishchannel
+ * @method{PATCH}
+ * @route {/v1/channel/openchannel-list/:channelId}
+ * 3. 관심채널 추가 또는 삭제
+ */
+router.patch('/add-or-remove-wishchannel', hasJsonWebToken, channelController.patchAddOrRemoveWishChannel);
 
-//POST /v1/channel/create
+
+/** GET /v1/channel/mychannels
+ * @method{GET}
+ * @route {/v1/channel/mychannels}
+ * 4. 해당유저의 채널 목록 조회
+ */
+router.get('/mychannels', hasJsonWebToken, channelController.getChannelListByUserId);
+
+
+/** POST /v1/channel/create
+ * @method{POST}
+ * @route {/v1/channel/create}
+ * 5. 채널 생성
+ */
 router.post('/create',
     hasJsonWebToken,
     multer({ storage: filesHandler.fileStorage, fileFilter: filesHandler.fileFilter, limits: { fieldSize: 25 * 1024 * 1024 } }).single('thumbnail'),
     filesHandler.saveUploadedChannelThumbnail,
-    channelController.postCreateChannel);// 3. 채널 생성
+    channelController.postCreateChannel);
 
-//GET /v1/channel/wishchannels
-router.post('/wishchannels', hasJsonWebToken, channelController.getWishChannelList); // 4. 관심 채널 조회 
 
-//GET /v1/channel/remove-wishchannel
-// router.patch('/remove-wishchannel', hasJsonWebToken, channelController.patchRemoveOpenChannelToWishChannel); // 5. 관심 채널 삭제 -> 1-2에 통합
+/** POST /v1/channel/wishchannels
+ * @method{POST}
+ * @route {/v1/channel/wishchannels} 
+ * 6. 해당유저의 관심채널 목록 조회
+ */
+router.post('/wishchannels', hasJsonWebToken, channelController.getWishChannelList); 
 
-//GET /v1/channel/:channelId
-router.get('/:channelId', hasJsonWebToken, channelController.getChannelDetailByChannelId);// 6. 채널아이디로 해당 채널 조회
 
-// PATCH /v1/channel/invite/:channelId
-router.patch('/invite/:channelId', hasJsonWebToken, channelController.patchInviteUserToChannel);// 6-1. 해당 채널에 유저 초대
+/** GET /v1/channel/:channelId
+ * @method{GET}
+ * @route {/v1/channel/:channelId} 
+ * 7. 해당채널의 세부정보 조회
+ */
+router.get('/:channelId', hasJsonWebToken, channelController.getChannelDetailByChannelId);
 
-// GET /v1/channel/:channelId/chat
-router.post('/:channelId/chat', hasJsonWebToken, channelController.getChatRoomListByChannelAndUserId);// 8. 채팅방 목록 조회
 
-// POST /v1/channel/:channelId/create-chatRoom
-router.post('/:channelId/create-chatRoom', hasJsonWebToken, channelController.postCreateChatRoom);// 9. 채팅방 생성
+/** PATCH /v1/channel/invite/:channelId
+ * @method{PATCH}
+ * @route {/v1/channel/invite/:channelId} 
+ * 8. 해당 채널에 유저 초대
+ */
+router.patch('/invite/:channelId', hasJsonWebToken, channelController.patchInviteUserToChannel);
 
-// POST /v1/channel/:channelId/create-workspace
-router.post('/:channelId/create-workspace', hasJsonWebToken, channelController.postCreateWorkSpace);// 10. 워크스페이스 생성
 
-// POST /v1/channel/:channelId/workspace
-router.post('/:channelId/workspace', hasJsonWebToken, channelController.getWorkSpaceListByChannelIdAndUserId);// 11. 워크스페이스 목록 조회
+/** POST /v1/channel/:channelId/chat
+ * @method{POST}
+ * @route {/v1/channel/:channelId/chat} 
+ * 9. 해당채널에서 유저가 속한 채팅룸 검색키워드로 목록 검색 
+ */
+router.post('/:channelId/chat', hasJsonWebToken, channelController.getChatRoomListByChannelAndUserId);
 
-//PATCH /v1/channel/exit/:channelId
-router.patch('/exit/:channelId', hasJsonWebToken, channelController.patchExitChannel);// 6-2. 채널 퇴장
 
-// POST /v1/channel/create-feed/:channelId
+/** POST /v1/channel/:channelId/create-chatRoom
+ * @method{POST}
+ * @route {/v1/channel/:channelId/create-chatRoom} 
+ * 10. 해당채널에 채팅룸 생성
+ */
+router.post('/:channelId/create-chatRoom', hasJsonWebToken, channelController.postCreateChatRoom);
+
+
+/** POST /v1/channel/:channelId/create-workspace
+ * @method{POST}
+ * @route {/v1/channel/:channelId/create-workspace} 
+ * 11. 해당채널에 워크스페이스 생성
+ */
+router.post('/:channelId/create-workspace', hasJsonWebToken, channelController.postCreateWorkSpace);
+
+
+/** POST /v1/channel/:channelId/workspace
+ * @method{POST}
+ * @route {/v1/channel/:channelId/workspace} 
+ * 12. 해당채널에서 유저가 속한 워크스페이스 검색키워드로 목록 검색 
+ */
+router.post('/:channelId/workspace', hasJsonWebToken, channelController.getWorkSpaceListByChannelIdAndUserId);
+
+
+/** PATCH /v1/channel/exit/:channelId
+ * @method{PATCH}
+ * @route {/v1/channel/exit/:channelId} 
+ * 13. 해당채널 퇴장
+ */
+router.patch('/exit/:channelId', hasJsonWebToken, channelController.patchExitChannel);
+
+
+/** POST /v1/channel/create-feed/:channelId
+ * @method{POST}
+ * @route {/v1/channel/create-feed/:channelId} 
+ * 14. 해당채널에 내 피드 생성
+ */
 router.post('/create-feed/:channelId',
     hasJsonWebToken,
     multer({ storage: filesHandler.fileStorage, fileFilter: filesHandler.fileFilter, limits: { fieldSize: 25 * 1024 * 1024 } }).array('files', 12),
     filesHandler.saveUploadedFiles,
-    channelController.postCreateFeedToChannel);// 20. 홈채널에 내피드 생성
+    channelController.postCreateFeedToChannel);
 
-// PATCH /v1/channel/edit-feed/:channelId/:feedId
+ 
+/** PATCH /v1/channel/edit-feed/:channelId/:feedId
+ * @method{PATCH}
+ * @route {/v1/channel/edit-feed/:channelId/:feedId} 
+ * 15. 해당채널에 내 피드 수정
+ */
 router.patch('/edit-feed/:channelId/:feedId',
     hasJsonWebToken,
     multer({ storage: filesHandler.fileStorage, fileFilter: filesHandler.fileFilter, limits: { fieldSize: 25 * 1024 * 1024 } }).array('files', 5),
     filesHandler.saveUploadedFiles,
     channelController.patchEditFeedToChannel);// 21. 홈채널에 내피드 수정
 
+
+/** DELETE /v1/channel/delete-feed/:channelId/:feedId
+ * @method{DELETE}
+ * @route {/v1/channel/delete-feed/:channelId/:feedId} 
+ * 16. 해당채널에 내 피드 삭제
+ */
 // DELETE /v1/channel/delete-feed/:channelId/:feedId
 router.delete('/delete-feed/:channelId/:feedId', hasJsonWebToken, channelController.deleteRemoveFeedByUserId);// 22. 홈채널에 내피드 삭제
 
-// PATCH /v1/channel/plus-or-minus-feed-like
-router.patch('/plus-or-minus-feed-like', hasJsonWebToken, channelController.patchPlusOrMinusNumberOfLikeInFeed);// 22. 홈채널에 내피드 삭제
 
-
+/** PATCH /v1/channel/plus-or-minus-feed-like
+ * @method{DELETE}
+ * @route {/v1/channel/plus-or-minus-feed-like} 
+ * 17. 피드 좋아요수 증가 또는 감소
+ */
+router.patch('/plus-or-minus-feed-like', hasJsonWebToken, channelController.patchPlusOrMinusNumberOfLikeInFeed);
 
 export default router;
