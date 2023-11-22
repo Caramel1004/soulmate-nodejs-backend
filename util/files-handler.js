5426110796e03f39d63ba436b0ccbdbd11a40335
Caramel1004 <Caramel1004.js@gmail.com>
2023-11-23 00:44:57 +0900

    내부 시스템에 파일 저장되는 코드: 현재 사용 안함

--- util/files-handler.js
+++ util/files-handler.js
@@ -31,28 +31,28 @@ const filesHandler = {
         try {
             const { files } = req.body;
             req.body.fileUrls = [];
-            if(req.body.existFileUrls) {
-                for(const fileUrl of req.body.existFileUrls) {
+            if (req.body.existFileUrls) {
+                for (const fileUrl of req.body.existFileUrls) {
                     req.body.fileUrls.push(fileUrl.name);
                 }
             }
-            if(files.length > 0) {
+            if (files.length > 0) {
                 // JSON형태로 되어있는 file 객체를 파싱하는 과정 -> buffer 프로퍼티의 data프로퍼티(배열 형태)값을 버퍼로 변환
                 const parsedFiles = JSON.parse(files, (key, value) => {
                     const parsedJson = value && value.type === 'Buffer' ? Buffer.from(value) : value;
                     return parsedJson;
                 });
-    
+
                 console.log('parsedFiles: ', parsedFiles);
                 // 두번째인수에 파일을 버퍼형태로 넣어줘야 파일 볼 수 있음
                 for (const parsedFile of parsedFiles) {
                     const fileId = new Date().getTime().toString(36);
                     const mimeType = parsedFile.mimetype.split('/')[1];
-    
+
                     fs.writeFileSync(`files/soulmate_Photo_${fileId}.${mimeType}`, parsedFile.buffer, () => {
                         console.log('파일 생성');
                     });
-    
+
                     req.body.fileUrls.push(`files/soulmate_Photo_${fileId}.${mimeType}`);
                 }
             }
@@ -68,19 +68,18 @@ const filesHandler = {
             const { photo } = req.body;
             req.body.fileUrls = [];
 
-            // JSON형태로 되어있는 file 객체를 파싱하는 과정 -> buffer 프로퍼티의 data프로퍼티(배열 형태)값을 버퍼로 변환
-            const parsedPhoto = JSON.parse(photo, (key, value) => {
-                const parsedJson = value && value.type === 'Buffer' ? Buffer.from(value) : value;
-                return parsedJson;
-            });
+            if (photo != 'undefined' && photo != 'null') {
+                // JSON형태로 되어있는 file 객체를 파싱하는 과정 -> buffer 프로퍼티의 data프로퍼티(배열 형태)값을 버퍼로 변환
+                const parsedPhoto = JSON.parse(photo, (key, value) => {
+                    const parsedJson = value && value.type === 'Buffer' ? Buffer.from(value) : value;
+                    return parsedJson;
+                });
 
-            console.log('parsedPhoto: ', parsedPhoto);
-            // 두번째인수에 파일을 버퍼형태로 넣어줘야 파일 볼 수 있음
-            for (const photo of parsedPhoto) {
+                // 두번째인수에 파일을 버퍼형태로 넣어줘야 파일 볼 수 있음
                 const fileId = new Date().getTime().toString(36);
-                const mimeType = photo.mimetype.split('/')[1];
-
-                fs.writeFileSync(`images/user_profiles/soulmate_Photo_${fileId}.${mimeType}`, photo.buffer, () => {
+                const mimeType = parsedPhoto.mimetype.split('/')[1];
+    
+                fs.writeFileSync(`images/user_profiles/soulmate_Photo_${fileId}.${mimeType}`, parsedPhoto.buffer, () => {
                     console.log('파일 생성');
                 });
                 req.body.fileUrls.push(`http://localhost:8080/images/user_profiles/soulmate_Photo_${fileId}.${mimeType}`);
@@ -97,7 +96,7 @@ const filesHandler = {
             console.log(req.body);
             const { thumbnail } = req.body;
             req.body.fileUrls = [];
-            if (thumbnail != 'undefined' && thumbnail!='null') {
+            if (thumbnail != 'undefined' && thumbnail != 'null') {
                 console.log('진입');
                 // JSON형태로 되어있는 file 객체를 파싱하는 과정 -> buffer 프로퍼티의 data프로퍼티(배열 형태)값을 버퍼로 변환
                 const parsedThumbnail = JSON.parse(thumbnail, (key, value) => {
