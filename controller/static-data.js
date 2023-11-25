1ac89736d292f5109fc8730dbd374719bcaf0e47
Caramel1004 <Caramel1004.js@gmail.com>
2023-11-26 00:13:35 +0900

    라우터 정리

--- controller/static-data.js
+++ controller/static-data.js
@@ -1,6 +1,10 @@
 import staticDataService from '../service/static-data.js'
 import { hasReturnValue } from '../validator/valid.js'
 
+/**
+ * 1. 채널 카테고리 조회
+ */
+
 export const getCategoryData = async (req, res, next) => {
     try {
         const data = await staticDataService.getCategoryData(next);
