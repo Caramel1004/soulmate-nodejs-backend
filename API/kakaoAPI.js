2567c1e857ac2b1ba7d349bb63093f2273b580ec
Caramel1004 <Caramel1004.js@gmail.com>
2023-11-29 10:05:24 +0900

    kakao API

--- API/kakaoAPI.js
+++ API/kakaoAPI.js
@@ -1,5 +1,9 @@
 import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+
 import { successType, temporaryRedirectType } from '../util/status.js';
+
+dotenv.config();
 
 const kakaoAPI = {
     // 1. 카카오 동의페이지
@@ -9,7 +13,6 @@ const kakaoAPI = {
                 method: 'GET',
             });
 
-            console.log(response);
             return {
                 url: response.url,
                 status: temporaryRedirectType.T03.t302
@@ -18,7 +21,7 @@ const kakaoAPI = {
             next(err);
         }
     },
-    // 4. 카카오에 토큰 요청
+    // 2. 카카오에 토큰 요청
     postRequestTokenToKakao: async (code, next) => {
         try {
             const response = await fetch(`https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${process.env.KAKAO_REST_API_KEY}&redirect_uri=${process.env.REDIRECT_URI}&code=${code}`, {
@@ -41,7 +44,7 @@ const kakaoAPI = {
             next(err);
         }
     },
-    // 5. 카카오 유저 정보 조회
+    // 3. 카카오 유저 정보 조회
     getRequestUserInfoByAccessToken: async (accessToken, next) => {
         try {
             const response = await fetch(`https://kapi.kakao.com/v2/user/me`, {
