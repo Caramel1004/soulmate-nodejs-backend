e7d6511ba1e27b284cf39cc56f62cda5ce8db430
Caramel1004 <Caramel1004.js@gmail.com>
2024-01-08 20:57:35 +0900

    cors origin uri 추가

--- socket.js
+++ socket.js
@@ -5,7 +5,7 @@ export default {
     init: server => {
         socketIO = new Server(server, {
             cors: {
-                origin: 'http://43.202.77.252:443',
+                origin: ['http://somate.site', 'http://www.somate.site'],
                 methods: ['GET, POST, PUT, PATCH, DELETE'],
                 allowedHeaders: ['Content-Type', 'application/json'],
                 credentials: true
