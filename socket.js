4b2772f8f59f336d426b0e6d434fa5835d70d242
Caramel1004 <Caramel1004.js@gmail.com>
2024-01-11 22:12:41 +0900

    cors origin https로 변경

--- socket.js
+++ socket.js
@@ -5,7 +5,7 @@ export default {
     init: server => {
         socketIO = new Server(server, {
             cors: {
-                origin: ['http://somate.site', 'http://www.somate.site'],
+                origin: ['https://somate.site', 'https://www.somate.site', 'http://localhost:4000'],
                 methods: ['GET, POST, PUT, PATCH, DELETE'],
                 allowedHeaders: ['Content-Type', 'application/json'],
                 credentials: true
